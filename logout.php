f1703644a8bcc2851393fec228024eaba5ba342e
rafal.pezda <smoktbg@gmail.com>
2024-04-25 14:47:55 +0200

    1.0

--- logout.php
+++ logout.php
@@ -2,6 +2,6 @@
 unset($_SESSION['user_id']);
 unset($_SESSION['imie_nazwisko']);
 unset($_SESSION['role']);
-header("Location: index.html"); // Przekierowanie na stronę po zalogowaniu
+header("Location: index.php"); // Przekierowanie na stronę po zalogowaniu
 exit;
 ?>
