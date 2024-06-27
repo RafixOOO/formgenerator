f04d61b49e6076a7fb6937d9801bfe7e1cab8083
rafal.pezda <smoktbg@gmail.com>
2024-06-27 13:58:21 +0200

    1.0

--- mail/verifyemail.php
+++ mail/verifyemail.php
@@ -170,7 +170,6 @@ $mail->Body = '<!DOCTYPE html>
 
   <!-- start preheader -->
   <div class="preheader" style="display: none; max-width: 0; max-height: 0; overflow: hidden; font-size: 1px; line-height: 1px; color: #fff; opacity: 0;">
-    A preheader is the short summary text that follows the subject line when an email is viewed in the inbox.
   </div>
   <!-- end preheader -->
 
